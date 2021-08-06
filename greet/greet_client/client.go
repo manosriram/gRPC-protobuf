328ac711e38d6dd24d96d47608578e03a6113514
Mano Sriram <mano.sriram0@gmail.com>
2021-08-06 19:24:59 +0530

    Add Server and Client Streaming to greet

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -4,10 +4,42 @@ import (
 	"context"
 	"fmt"
 	"grpc-ex/greet/greetpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
 )
+
+func greet_driver(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetRequest{
+		Greet: &greetpb.Greeting{
+			FirstName: "Mano",
+			LastName:  "Sriram",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Err while calling Greet() rpc %v", err)
+	}
+	log.Printf("Response from Greet() %+v", res)
+}
+
+func greet_many_times_driver(c greetpb.GreetServiceClient) {
+	greet_many_request := &greetpb.GreetManyTimesRequest{
+		Greet: &greetpb.Greeting{
+			FirstName: "Mano",
+			LastName:  "Sriram",
+		},
+	}
+	res_stream, _ := c.GreetManyTimes(context.Background(), greet_many_request)
+	for {
+		msg, err := res_stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(msg)
+	}
+}
 
 func main() {
 	fmt.Println("This is client")
@@ -20,19 +52,5 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-
-	req := &greetpb.GreetRequest{
-		Greet: &greetpb.Greeting{
-			FirstName: "Mano",
-			LastName:  "Sriram",
-		},
-	}
-	res, err := c.Greet(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Err while calling Greet() rpc %v", err)
-	}
-	log.Printf("Response from Greet() %+v", res)
-
-	// fmt.Printf("Client created %f", c)
-
+	greet_many_times_driver(c)
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	greetpb "grpc-ex/greet/greetpb"
 
@@ -23,6 +25,21 @@ func (s *server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb
 		Result: result,
 	}
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes() was invoked with %v", req)
+	first_name := req.GetGreet().GetFirstName()
+	for t := 0; t < 10; t++ {
+		result := "Hello, " + first_name + " with iteration " + strconv.Itoa(t)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	return nil
 }
 
 func main() {
